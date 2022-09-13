8783530311af011907d1ec55f8f3eff1ac082b71
jaybjackie <kodchakan.forwork@gmail.com>
2022-09-13 19:31:13 +0700

    Resolved many issues

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from time import time
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
@@ -7,7 +8,8 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+    end_date = models.DateTimeField('date end', null=True)
+
     def __str__(self):
         return self.question_text
     
@@ -20,6 +22,21 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        return (timezone.now() >= self.pub_date)
+        
+    def can_vote(self):
+        time = timezone.now()
+        if self.is_published():
+            # Question had published
+            if self.end_date is None:
+                # Null end date
+                return True
+            else:
+                return (time <= self.end_date)
+        else:
+            return False
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
+from email import message
 from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.contrib import messages
 from .models import Choice, Question
 
 
@@ -23,13 +24,23 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+ 
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+    def get_question(self, request):
+        """If question doesn't exist or closed, redirect to index page."""
+        try:
+            self.object = self.get_object()
+            return self.render_to_response(self.get_context_data(object=self.object))
+        except:
+            messages.error(request, 'Question is unavailiable')
+            return HttpResponseRedirect(reverse('polls:index'))
+
+            
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
@@ -37,6 +48,9 @@ class ResultsView(generic.DetailView):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
+    if not question.can_vote():
+        return HttpResponseRedirect(reverse('polls:index'))
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
